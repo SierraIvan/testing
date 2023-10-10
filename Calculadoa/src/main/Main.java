77a7d6ce33d419cb02d3c96b5afb6ff3a4b68207
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-10-10 18:39:36 +0200

    primer commit

---
+++ Calculadoa/src/main/Main.java
@@ -0,0 +1,46 @@
+package main;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double numero1, numero2, resultado;
+        char operador;
+
+        System.out.print("Ingrese el primer número: ");
+        numero1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el operador (+, -, *, /): ");
+        operador = scanner.next().charAt(0);
+
+        System.out.print("Ingrese el segundo número: ");
+        numero2 = scanner.nextDouble();
+
+        switch (operador) {
+            case '+':
+                resultado = numero1 + numero2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '/':
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Error: División por cero no permitida.");
+                }
+                break;
+            default:
+                System.out.println("Operador no válido.");
+        }
+
+        scanner.close();
+    }
+}
